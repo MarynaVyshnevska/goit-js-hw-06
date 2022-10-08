316393ea52c3e5aa4118adbdacf7f87f029f1066
Maryna Vyshnevska <vyshnevska.maryna@gmail.com>
2022-10-08 23:04:35 +0300

    complete 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)}`;
-};
+const btnRef = document.querySelector('.change-color');
+const styleWindowRef = document.querySelector('body');
+const codeColorRef = document.querySelector('.color');
 
+
+btnRef.addEventListener('click', () => getRandomHexColor());
+  
+function getRandomHexColor(event) {
+  const newColor = "#" + Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)
+    .toLowerCase();
+  
+  console.log('new Background color:', newColor);
+  codeColorRef.textContent = newColor;
+  styleWindowRef.style.backgroundColor = newColor;
+}
